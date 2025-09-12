02d085971c4892ae4fd997f087608801b6ebd698
TetianaPakhomova <godibrigon99@gmail.com>
2025-09-12 10:11:07 +0200

    domain implement

---
+++ src/main/java/app/repository/MemberRepository.java
@@ -0,0 +1,39 @@
+package app.repository;
+
+import app.domain.Member;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MemberRepository {
+    private final Map<Long, Member> database = new HashMap<>();
+    private long maxId;
+
+
+    public Map<Long, Member> getDatabase() {
+        return database;
+    }
+
+    public Member save(Member member) {
+        member.setId(++maxId);
+        database.put(maxId, member);
+        return member;
+    }
+
+    public List<Member> findAll() {
+        return new ArrayList<>(database.values());
+
+    }
+    public Member findById(Long id) {
+        return database.get(id);
+
+    }
+    public void deleteById (Long id) {
+        database.remove(id);
+
+    }
+
+
+}
